807d05d59c8ccacb4398a855818f9fe6a1c3c3cc
Tero Nurminen <tero.nurminen@gmail.com>
2010-10-15 11:32:06 +0300

    Harjoitustehtävä 1
    Lisätään tomorrow(), jota käytetään debugattaessa päivämäärän muuttumista.

--- src/fi/turkuamk/examples/Turkuamk_gae_examplesServlet.java
+++ src/fi/turkuamk/examples/Turkuamk_gae_examplesServlet.java
@@ -1,6 +1,12 @@
 package fi.turkuamk.examples;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
@@ -8,6 +14,21 @@ public class Turkuamk_gae_examplesServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		resp.getWriter().print("Tomorrow is " + tomorrow());
+	}
+
+	public String tomorrow() {
+		Calendar cal = Calendar.getInstance(TimeZone
+				.getTimeZone("Europe/Helsinki"));
+		Date date = cal.getTime();
+		System.out.println(date.toString());
+
+		cal.add(Calendar.DAY_OF_MONTH, 1);
+		date = cal.getTime();
+		System.out.println(date.toString());
+
+		DateFormat dfm = new SimpleDateFormat("EEEE");
+
+		return dfm.format(date).toString();
 	}
 }
